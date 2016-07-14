52206cd03a2782f5c9148892221d10d15927024c
Vladimir Li <vladde1991@gmail.com>
2016-07-14 16:56:17 +0200

    working line_err_estimator

--- src/line_err_estimator.py
+++ src/line_err_estimator.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from __future__ import print_function
+from __future__ import division
 import roslib
 import sys
 import rospy
@@ -23,11 +24,11 @@ class image_converter:
       cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
     except CvBridgeError as e:
       print(e)
+      # Convert BGR to HSV
 
     (rows,cols,channels) = cv_image.shape
-
-    # Convert BGR to HSV
     hsv = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
+
 
 
 
@@ -45,11 +46,40 @@ class image_converter:
 
     # Canny edge and Hough transform
     mask = cv2.Canny(mask, 10, 100)
-    lines = cv2.HoughLines(mask, 1,np.pi/180,100)
+    lines = cv2.HoughLines(mask, 5,np.pi/90,200)
+
     if lines is not None:
-        lines = np.mean(lines, axis=0)
-        theta = lines[0][1]
-        rho = 720/2-lines[0][0]
+        for rho,theta in lines[0]:
+            a = np.cos(theta)
+            b = np.sin(theta)
+            x0 = a*rho
+            y0 = b*rho
+            x1 = int(x0 + 1000*(-b))
+            y1 = int(y0 + 1000*(a))
+            x2 = int(x0 - 1000*(-b))
+            y2 = int(y0 - 1000*(a))
+            if (x1-x2)!=0:
+                k = (y2-y1)/(x2-x1)
+                m = y1-k*x1
+            else:
+                k = float("inf")
+                m = float("inf")
+        y_c = 180
+        y_0 = 0
+        if k!=float("inf") and k!=0:
+            x_c = (y_c-m)/k
+            x_0 = (y_0-m)/k
+        else:
+            x_c = x1
+            x_0 = x1
+
+        cv2.line(cv_image,(int(x_c),int(y_c)),(int(x_0),int(y_0)),(0,0,255),2)
+
+        if k!=0:
+            theta = 1/k
+        else:
+            theta = 0
+        rho = 640/2-x_c
         if theta > np.pi/2:
             theta = -(np.pi-theta)
         err_twist = Twist()
@@ -57,14 +87,18 @@ class image_converter:
         err_twist.linear.y = rho
         self.err_pub.publish(err_twist)
 
-    #cv2.imshow("Image window", cv_image)
-    #cv2.waitKey(3)
+
+
+
+    cv2.imshow("Image window", cv_image)
+    cv2.waitKey(3)
 
 
 
 def main(args):
-  ic = image_converter()
   rospy.init_node('line_err_estimator', anonymous=True)
+  ic = image_converter()
+
 
   try:
     rospy.spin()
